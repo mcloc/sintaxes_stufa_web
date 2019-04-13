c14f5c7e6bcee0ab2172980ef408f02f4b8af498
Mario Caseiro <mcl.caseiro@gmail.com>
2019-04-13 02:56:00 -0300

    changed main layout to page chart js which will be used as dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,129 @@
 
 @section('title', 'Sintechs Admin - Home')
 
-@section('breadcrumbs')
+@section('page_name')
     <h1>Dashboard</h1>
 @endsection
-
-
-
-
-@section('alert-message')
-<span class="badge badge-pill badge-success">Success</span> You successfully read this important alert message.ction('content')
+@section('breadcrumbs')
+<li><a href="#">Dashboard</a></li>
+<li><a href="#">Charts</a></li>
+<li class="active">Chartjs</li>
 @endsection
+
+
+<!--  TODO: change this div col-sm-12 to mainlayout with a if statement for messages
+      and do a yield with only the <span> message
+ -->
+@section('alert-message')
+ <div class="col-sm-12">
+    <div class="alert  alert-success alert-dismissible fade show" role="alert">
+        <span class="badge badge-pill badge-success">Success</span> You successfully read this important alert message.ction('content')
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</div>		
+@endsection
+
 
 
 @section('content')
-	@parent
+            <div class="animated fadeIn">
+                <div class="row">
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Yearly Sales </h4>
+                                <canvas id="sales-chart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Team Commits </h4>
+                                <canvas id="team-chart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Bar chart </h4>
+                                <canvas id="barChart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Rader chart </h4>
+                                <canvas id="radarChart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Line Chart </h4>
+                                <canvas id="lineChart"></canvas>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="mb-3">Doughut Chart </h4>
+                                    <canvas id="doughutChart"></canvas>
+                                </div>
+                            </div>
+                        </div><!-- /# column -->
+
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Pie Chart </h4>
+                                <canvas id="pieChart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Polar Chart </h4>
+                                <canvas id="polarChart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="mb-3">Single Bar Chart </h4>
+                                <canvas id="singelBarChart"></canvas>
+                            </div>
+                        </div>
+                    </div><!-- /# column -->
+
+                </div>
+            </div><!-- .animated -->
+@endsection
+
+@section('final-includes')
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <!--  Chart js -->
+    <script src="vendors/chart.js/dist/Chart.bundle.min.js"></script>
+    <script src="assets/js/init-scripts/chart-js/chartjs-init.js"></script>
 @endsection
