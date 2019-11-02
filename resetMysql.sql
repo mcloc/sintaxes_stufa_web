5fd9f34bb89ba8990928b9bf1b268a278513f438
Mario Caseiro <mcloc@katharine.sintaxes,com.br>
2019-11-01 23:02:49 -0300

    change on resetMySQL.sql

--- resetMysql.sql
+++ resetMysql.sql
@@ -1,4 +1,4 @@
 drop database if exists vger;
 create database vger;
-grant all PRIVILEGES on vger.* to 'vger'@'localhost';
+grant all PRIVILEGES on vger.* to 'vger'@'localhost' IDENTIFIED BY '12345678';
 FLUSH PRIVILEGES;
